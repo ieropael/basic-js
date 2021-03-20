913f19e4e7de7c2431ea29ab637433002232516e
Aleksandr Petrov <ieropael@gmail.com>
2021-03-20 15:50:47 +0400

    feat: add "Chain maker" task solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,28 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.push('~( ' + value + ' )~');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.arr.length) {
+      this.arr = [];
+      throw new Error();
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let string = this.arr.join('').slice(1, -1);
+    this.arr = [];
+    return string;
   }
 };
 
